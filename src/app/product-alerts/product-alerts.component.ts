c14f725bc98371d50110414fef9a53a923a0b1b7
Conarius <maxretzlaff13@gmail.com>
2022-03-16 09:08:03 +0100

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,16 +8,12 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
-  }
-
+  } 
   
 }
 
-export class ProductAlertsComponent {
-  @Input() product: Product | undefined;
-  @Output() notify = new EventEmitter();
-}
